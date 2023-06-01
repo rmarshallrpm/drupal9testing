26cd3de4a12b7521f7c1d0c31c8c8923462e5a03
Roger Marshall <rmarshallrpm@gmail.com>
2023-06-01 14:52:09 -0700

    Settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -725,6 +725,11 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * @see https://www.drupal.org/docs/installing-drupal/trusted-host-settings
  */
 
+ $settings['trusted_host_patterns'] = [
+  '^local.drupal9\.com$',
+];
+
+
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
